2fd2b883c911f199e3492790f786059de337498b
Alex Kozhenov <kozhenov1984@mail.ru>
2023-10-29 22:10:11 +0400

    feat: add main components

---
+++ src/components/Result.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+export default class Result extends Component {
+  render() {
+    return (
+      <div className='result'>
+        Result
+      </div>
+    )
+  }
+}
---
+++ src/components/Search.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+export default class Search extends Component {
+  render() {
+    return (
+      <div className='search'>
+        Search
+      </div>
+    )
+  }
+}
